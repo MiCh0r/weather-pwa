8cc9746fd095e914fbf72d4a9d88c2d34ee30a6f
Simon Planberger <simon.planberger@atn.nu>
2020-11-18 18:01:06 +0100

    Reorder event

--- weather-pwa/src/app/home/home.page.ts
+++ weather-pwa/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemReorderEventDetail } from '@ionic/core';
 
 export interface IWeatherLocation {
   location: string;
@@ -32,5 +33,19 @@ export class HomePage implements OnInit {
 
   public removeWeatherCard(): void {
     this.weatherData.pop();
+  }
+
+  public doReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // Before complete is called with the items they will remain in the
+    // order before the drag
+    console.log('Before complete', this.weatherData);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. Update the items variable to the
+    // new order of items
+    this.weatherData = ev.detail.complete(this.weatherData);
+
+    // After complete is called the items will be in the new order
+    console.log('After complete', this.weatherData);
   }
 }
